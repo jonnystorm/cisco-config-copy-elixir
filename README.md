c04de9228441416159133ce69456e85010651967
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2015-04-20 22:31:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+CiscoConfigCopy
+=================
+
+Module providing CISCO-CONFIG-COPY-MIB functionality for use with [net-snmp-elixir](https://github.com/jonnystorm/net-snmp-elixir).
---
+++ mix.exs
@@ -0,0 +1,20 @@
+defmodule CiscoConfigCopy.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :cisco_config_copy_ex,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  defp deps do
+    [{:snmp_mib_elixir, git: "https://github.com/jonnystorm/snmp-mib-elixir"}]
+  end
+end
